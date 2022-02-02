82a3bd768c57c2994c1e75dfb87f4a17331e3bba
tojiboyevf <faxri1997@mail.ru>
2022-02-03 00:45:48 +0300

    Folders

--- STORM-IRIT/run.sh
+++ STORM-IRIT/run.sh
@@ -2,16 +2,16 @@
 
 set -ex
 
-filename="${1%.*}"
+base_name=$(basename -- "$1")
+filename="${base_name%.*}"
 
-./pdpcComputeMultiScaleFeatures -i input/$1 -v -o output/$filename
-./pdpcSegmentation -i input/$1 -s output/"${filename}_scales.txt" -f output/"${filename}_features.txt" -o output/$filename -v
+./pdpcComputeMultiScaleFeatures -i input/$base_name -v -o output/$filename
+./pdpcSegmentation -i input/$base_name -s output/"${filename}_scales.txt" -f output/"${filename}_features.txt" -o output/$filename -v
 
-mkdir output/range output/pers output/scales
+mkdir output/"${filename}_range" output/"${filename}_pers" output/"${filename}_scales"
 
-./pdpcPostProcess -i input/$1 -s output/"${filename}_seg.txt" -c output/"${filename}_comp.txt" -o output/range/$filename -col -v -range 20 24 25 30 40 42
+./pdpcPostProcess -i input/$base_name -s output/"${filename}_seg.txt" -c output/"${filename}_comp.txt" -o output/"${filename}_range"/$filename -col -v -range 20 24 25 30 40 42
 
-./pdpcPostProcess -i input/$1 -s output/"${filename}_seg.txt" -c output/"${filename}_comp.txt" -o output/pers/$filename -col -v -pers 15 20 25
+./pdpcPostProcess -i input/$base_name -s output/"${filename}_seg.txt" -c output/"${filename}_comp.txt" -o output/"${filename}_pers"/$filename -col -v -pers 15 20 25
 
-./pdpcPostProcess -i input/$1 -s output/"${filename}_seg.txt" -c output/"${filename}_comp.txt" -o output/scales/$filename -col -v -scales 5 15 20 25
-
+./pdpcPostProcess -i input/$base_name -s output/"${filename}_seg.txt" -c output/"${filename}_comp.txt" -o output/"${filename}_scales"/$filename -col -v -scales 5 15 20 25
