f6516d1cedce13f1804dbc6f00f6bfe3589857ba
tojiboyevf <faxri1997@mail.ru>
2022-02-11 07:12:51 +0300

    Due to gitignore config one file wasn't added. I added this file

---
+++ DDPFF/scripts/build_cloud_from_planes.py
@@ -0,0 +1,28 @@
+import sys
+import numpy as np
+import open3d as o3d
+
+
+if __name__ == '__main__':
+
+    path = 'result.ply'
+    pcd = o3d.io.read_point_cloud(path)
+
+    planes = []                
+    with open('planes.txt', 'r') as f:
+        for line in f:
+            planes.append(np.asarray([int(x) for x in line.split()]))
+    
+        
+    colors = np.array(pcd.colors)
+
+    for plane_indices in planes:
+        col = np.random.uniform(0,1, size=(1,3))
+        if plane_indices.size > 0:
+            colors[plane_indices] = col
+
+    pcd.colors = o3d.utility.Vector3dVector(colors)
+
+    filename = sys.argv[1].split('.')[0]
+
+    o3d.io.write_point_cloud('output/' + filename + '_result.pcd', pcd)
