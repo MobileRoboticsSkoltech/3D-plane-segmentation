5032e7d80d7ff5a73a022ec8e11210fdab4b0ebc
p0l0satik <denis.cucu@yandex.ru>
2022-02-14 18:15:58 +0300

    moved tmp files reduced layers in dockerfile

--- STORM-IRIT/scripts/convert_txt.py
+++ STORM-IRIT/scripts/convert_txt.py
@@ -4,6 +4,7 @@ import numpy as np
 
 if __name__ == '__main__':
     arr = np.loadtxt(sys.argv[1], delimiter="\n")
+    arr += 1
     filename = sys.argv[1].split('.')[0]
     np.save(filename + ".npy", arr)
     os.remove(sys.argv[1])
--- STORM-IRIT/scripts/run.sh
+++ STORM-IRIT/scripts/run.sh
@@ -8,18 +8,15 @@ process(){
     path=output/$filename
     
     python3 preprocess_cloud.py $base_name
-    ./pdpcComputeMultiScaleFeatures -i "${filename}.ply" -v -o $path/$filename
-    ./pdpcSegmentation -i "${filename}.ply" -s $path/"${filename}_scales.txt" -f $path/"${filename}_features.txt" -o $path/$filename -v
+    ./pdpcComputeMultiScaleFeatures -i "${filename}.ply" -v -o $filename
+    ./pdpcSegmentation -i "${filename}.ply" -s "${filename}_scales.txt" -f "${filename}_features.txt" -o $filename -v
 
-    ./pdpcPostProcess -i "${filename}.ply" -s $path/"${filename}_seg.txt" -c $path/"${filename}_comp.txt" -o $path/"${filename}_range" -col -v -range 20 24 25 30 40 42
+    ./pdpcPostProcess -i "${filename}.ply" -s "${filename}_seg.txt" -c "${filename}_comp.txt" -o $path/"${filename}_range" -col -v -range 20 24 25 30 40 42
 
-    ./pdpcPostProcess -i "${filename}.ply" -s $path/"${filename}_seg.txt" -c $path/"${filename}_comp.txt" -o $path/"${filename}_pers" -col -v -pers 15 20 25
+    ./pdpcPostProcess -i "${filename}.ply" -s "${filename}_seg.txt" -c "${filename}_comp.txt" -o $path/"${filename}_pers" -col -v -pers 15 20 25
 
-    ./pdpcPostProcess -i "${filename}.ply" -s $path/"${filename}_seg.txt" -c $path/"${filename}_comp.txt" -o $path/"${filename}_scales" -col -v -scales 5 15 20 25
-    rm $path/"${filename}_scales.txt"
-    rm $path/"${filename}_features.txt"
-    rm $path/"${filename}_seg.txt"
-    rm $path/"${filename}_comp.txt"
+    ./pdpcPostProcess -i "${filename}.ply" -s "${filename}_seg.txt" -c "${filename}_comp.txt" -o $path/"${filename}_scales" -col -v -scales 5 15 20 25
+
     for f in $path/*.ply; do
         python3 convert_ply.py $f
     done
