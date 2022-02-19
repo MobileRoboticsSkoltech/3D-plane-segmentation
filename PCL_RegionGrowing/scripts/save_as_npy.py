c2d3e8cc72ec67d1177fa0fa2d38a8b1a3231c47
pmokeev <pav3l.mokeev@gmail.com>
2022-02-19 21:29:00 +0300

    Added script which converts txt to npy

---
+++ PCL_RegionGrowing/scripts/save_as_npy.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+
+
+def convert_txt_files_to_npy_format():
+    rootdir = "../output"
+
+    for subdir, _, files in os.walk(rootdir):
+        for file in files:
+            file_path = os.path.join(subdir, file)
+            labels_array = []
+            with open(file_path, "r") as file:
+                lines = file.readlines()
+                for line in lines:
+                    labels_array.append(int(line))
+
+            np.save(file_path[:-4] + ".npy", np.array(labels_array))
+            os.remove(file_path)
+
+
+if __name__ == "__main__":
+    convert_txt_files_to_npy_format()
