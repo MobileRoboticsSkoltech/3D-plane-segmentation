c39334bb0556f94e25fc59f851fb1786b38cb360
p0l0satik <Denis.Kukushkin@skoltech.ru>
2022-02-26 18:15:56 +0300

    fixed unsegmented areas

--- peac/scripts/store_colors.py
+++ peac/scripts/store_colors.py
@@ -6,10 +6,9 @@ from pypcd import pypcd
 if __name__ == '__main__':
     pc = pypcd.PointCloud.from_path(sys.argv[1])
     colors =  pc.pc_data["rgb"]
-    colors_map = {}
     for n, color in enumerate(np.unique(colors)):
-        colors_map[color] = n
-    for n, color in enumerate(np.unique(colors)):
+        if color == 0:
+            colors[np.where(colors == color)] = 0
         colors[np.where(colors == color)] = n + 1
 
     filename = sys.argv[1].split('.')[0]
